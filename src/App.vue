c27519ffadc017445b96c778beb9d56a6b0c08f1
RDL <1366569467@qq.com>
2020-10-25 09:37:38 +0800

    迭代

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
-   
+  	<router-view></router-view>
+ 	<MainTabar></MainTabar>
   </div>
 </template>
 
 <script>
-
+import MainTabar from 'components/content/maintabar/MainTabar'
 export default {
   name: 'App',
   components: {
-   
+   	MainTabar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/css/base.css";
 </style>
